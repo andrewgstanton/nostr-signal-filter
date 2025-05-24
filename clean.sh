c7167adedd4280a5efb1c5a68a5e2b118d472546
Andrew Stanton <andrewgstanton@gmail.com>
2025-05-23 21:51:36 -0700

    made different options in clean.sh so can be templated if need be (to add flags later)

--- clean.sh
+++ clean.sh
@@ -1,1 +1,13 @@
-docker rmi -f $(docker images -a -q)
+#/bin/bash
+
+echo "Stopping all running containers..."
+docker stop $(docker ps -aq) 2>/dev/null
+
+echo "Removing all containers..."
+docker rm $(docker ps -aq) 2>/dev/null
+
+echo "Removing all images..."
+docker rmi $(docker images -q) 2>/dev/null
+
+echo "Docker cleanup complete."
+	
